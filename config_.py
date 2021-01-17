733705ba0045936017e6d69ec0892c669872f840
pythontzy <61105590+pythontzy@users.noreply.github.com>
2021-01-17 20:44:30 +0800

    Add files via upload

--- config_.py
+++ config_.py
@@ -10,6 +10,7 @@ __C                           = edict()
 cfg                           = __C
 
 __C.data_file = "./data/cn_train.txt" # 原文
-__C.batch_size = 16
+__C.data_file_v2 = "./data/cn_train_v2.txt" # 原文
+__C.batch_size = 4
 __C.epoch = 10
 __C.lr_rate = 1e-3
