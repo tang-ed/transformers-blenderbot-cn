7eb843dc76aaf3facfca5045cf85c8d2f69edb0e
pythontzy <61105590+pythontzy@users.noreply.github.com>
2021-01-27 15:21:40 +0800

    Add files via upload

--- pre_train.py
+++ pre_train.py
@@ -1,21 +1,23 @@
-from transformers import TFBlenderbotSmallForConditionalGeneration, BlenderbotSmallTokenizer
+from transformers import TFBlenderbotSmallModel, BlenderbotSmallTokenizer, BlenderbotSmallConfig
 import tensorflow as tf
 from tensorflow import keras
 from config_ import cfg
-from typing import List
+import numpy as np
 
 
 mname = 'facebook/blenderbot_small-90M'
-blen_model = TFBlenderbotSmallForConditionalGeneration.from_pretrained(mname)
+blen_model = TFBlenderbotSmallModel.from_pretrained(mname)
 tokenizer = BlenderbotSmallTokenizer.from_pretrained(mname)
+config = BlenderbotSmallConfig.from_pretrained(mname)
+
 
 que = []
 ans = []
-start_token = "__start__"
-end_token = "__end__"
+start_token = 1
+end_token = 2
 
 
-with open(cfg.data_file_v2) as f:
+with open(cfg.data_file) as f:
     data = f.read().split("\n")
 
     state = None
@@ -23,13 +25,14 @@ with open(cfg.data_file_v2) as f:
     a = None
 
     for i in range(len(data)):
-        if data[i] != "":
-            if data[i + 1] != "":
+        if data[i] != " " and data[i] != "":
+            if data[i + 1] != "" and data[i+1] != " ":
                 if q is None:
                     q = data[i]
                     a = data[i+1]
                     que.append(q)
-                    ans.append(start_token + " " + a)
+                    ans.append(a)
+
                     state = q+" | " + a
                     continue
                 if state is not None:
@@ -37,7 +40,8 @@ with open(cfg.data_file_v2) as f:
                     a = data[i + 1]
                     state = q + " | " + a
                     que.append(q)
-                    ans.append(start_token + " " + a)
+                    ans.append(a)
+
 
             else:
                 continue
@@ -47,18 +51,32 @@ with open(cfg.data_file_v2) as f:
             a = None
 
 inps = tokenizer(que, return_tensors="tf", padding=True, truncation=True)
-outps = tokenizer(ans, return_tensors="tf", padding=True, truncation=True)
+outputs = tokenizer(ans, return_tensors="tf", padding=True, truncation=True, max_length=126)
+
+ans_datas = outputs["input_ids"]
+
+new_ans = []
+for i in ans_datas:
+    ans_ls = list(i.numpy())
+    ans_ls.insert(0, start_token)
+    try:
+        z_index = ans_ls.index(0)
+        ans_ls.insert(z_index, end_token)
+    except:
+        ans_ls.insert(-1, end_token)
+    new_ans.append(ans_ls)
+
+ans_data = np.array(new_ans)
 
 input_ids = inps["input_ids"]
 
 input_mask = inps["attention_mask"]
-output_ids = outps["input_ids"]
-output_mask = outps["attention_mask"]
+output_ids = ans_data
+output_mask = np.array(output_ids > 0, dtype="int32")
 
 inp_shape = input_ids.shape[1]
 out_shape = output_ids.shape[1]
-print(input_ids.shape[0])
-exit()
+
 
 
 class NaturalExpDecay(tf.keras.optimizers.schedules.LearningRateSchedule):
@@ -72,67 +90,45 @@ class NaturalExpDecay(tf.keras.optimizers.schedules.LearningRateSchedule):
     def __call__(self, step):
         return self.initial_learning_rate * tf.math.exp(-self.decay_rate * (step / self.decay_steps))
 
-
-
-
-
-def shape_list(tensor: tf.Tensor) -> List[int]:
-
-    dynamic = tf.shape(tensor)
-
-    if tensor.shape == tf.TensorShape(None):
-        return dynamic
-
-    static = tensor.shape.as_list()
-
-    return [dynamic[i] if s is None else s for i, s in enumerate(static)]
-
-def compute_loss(labels, logits):
-
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True,
-        reduction=tf.keras.losses.Reduction.NONE,
-    )
-    melted_labels = tf.reshape(labels, (-1,))
-    active_loss = tf.not_equal(melted_labels, 0)
-    reduced_logits = tf.boolean_mask(tf.reshape(logits, (-1, shape_list(logits)[2])), active_loss)
-    labels = tf.boolean_mask(melted_labels, active_loss)
-    return loss_fn(labels, reduced_logits)
-
-
 def models():
     inp = keras.layers.Input(shape=[inp_shape], dtype="int64")
-    de_out = keras.layers.Input(shape=[out_shape], dtype="int64")
+    de_out = keras.layers.Input(shape=[out_shape-1], dtype="int64")
     inp_mask = keras.layers.Input(shape=[inp_shape], dtype="int32")
-    out_mask = keras.layers.Input(shape=[out_shape], dtype="int32")
-    out = blen_model(input_ids=inp, decoder_input_ids=de_out, attention_mask=inp_mask, decoder_attention_mask=out_mask, training=True, use_cache=False)[0]
-    return keras.models.Model(inputs=[inp, de_out, inp_mask, out_mask], outputs=out)
+    out_mask = keras.layers.Input(shape=[out_shape-1], dtype="int32")
+    out = blen_model(input_ids=inp, decoder_input_ids=de_out, attention_mask=inp_mask, decoder_attention_mask=out_mask, training=True)[0]
+    logits = keras.layers.Dense(config.vocab_size)(out)
+
+    return keras.models.Model(inputs=[inp, de_out, inp_mask, out_mask], outputs=logits), blen_model
 
 
 def train():
 
-    train_dataset = tf.data.Dataset.from_tensor_slices(({
-                                                            'input_1': input_ids,
-                                                            'input_2': output_ids,
-                                                            'input_3': input_mask,
-                                                            'input_4': output_mask,
-                                                        },  output_ids))
-    train_dataset = train_dataset.shuffle(1000).batch(cfg.batch_size)
+    dataset = ({
+            'input_1': input_ids,
+            'input_2': output_ids[:, :-1],
+            'input_3': input_mask,
+            'input_4': output_mask[:, :-1],
+            }, output_ids[:, 1:])
 
-    total_steps = inps["input_ids"].shape[0] // cfg.batch_size * cfg.epoch
+    train_dataset = tf.data.Dataset.from_tensor_slices(dataset)
+    train_dataset = train_dataset.shuffle(2000).batch(cfg.batch_size)
+
+    total_steps = input_ids.shape[0] // cfg.batch_size * cfg.epoch
     print("总步数：", total_steps)
-
     natural_exp_decay = NaturalExpDecay(initial_learning_rate=cfg.lr_rate,
                                         decay_steps=total_steps,
-                                        decay_rate=1e-7)
+                                        decay_rate=1e-6)
 
     optimizer = keras.optimizers.Adam(natural_exp_decay)
-    model = models()
-    model.compile(optimizer=optimizer, loss=compute_loss, metrics=["accuracy"])
+    model, b_model = models()
+    model.compile(optimizer=optimizer, loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE), metrics=["accuracy"])
     model.summary()
 
+
     model.fit(train_dataset, epochs=cfg.epoch)
-    model.save("tf_model.h5")
+    model.save_weights("tf_model.h5")
+    blen_model.save_pretrained("blen_model")
+
 
 
 if __name__ == '__main__':
