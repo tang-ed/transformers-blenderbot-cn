9462577beec69799420a3facfb51a6a8fde3e0f9
tang-py-tens <61105590+tang-py-tens@users.noreply.github.com>
2021-07-31 00:50:31 +0800

    Add a break flag.

--- create_json.py
+++ create_json.py
@@ -5,7 +5,7 @@ import json
 tokenizer = SelfTokenizer()
 
 
-vocab_size = tokenizer.train(["./data/train_data.txt"], special_tokens=["__null__", "__start__", "__end__", "__unk__"])
+vocab_size = tokenizer.train(["./data/train_data.txt"], special_tokens=["__null__", "__start__", "__end__","|" , "__unk__"])
 tokenizer.save("vocab")
 
 with open("model_file/config_small.json", "r") as f:
